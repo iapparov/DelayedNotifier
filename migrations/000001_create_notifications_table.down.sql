7f40f58d1d884feda0711d7333988c0423f56872
Emil Iapparov <emealeaque@gmail.com>
2025-11-06 14:31:41 +0500

    FIX: migrations

--- migrations/000001_create_notifications_table.down.sql
+++ migrations/000001_create_notifications_table.down.sql
@@ -1,10 +1,1 @@
-CREATE TABLE IF NOT EXISTS notifications (
-    id          UUID PRIMARY KEY,
-    channel     TEXT NOT NULL,
-    recipient   TEXT NOT NULL,
-    message     TEXT NOT NULL,
-    send_at     TIMESTAMP NOT NULL,
-    status      TEXT NOT NULL, -- pending, sent, failed, canceled
-    created_at  TIMESTAMP NOT NULL DEFAULT now(),
-    updated_at  TIMESTAMP NOT NULL DEFAULT now()
-);
+DROP TABLE IF EXISTS notifications;
--- migrations/000001_create_notifications_table.up.sql
+++ migrations/000001_create_notifications_table.up.sql
@@ -1,1 +1,10 @@
-DROP TABLE IF EXISTS notifications;
+CREATE TABLE IF NOT EXISTS notifications (
+    id          UUID PRIMARY KEY,
+    channel     TEXT NOT NULL,
+    recipient   TEXT NOT NULL,
+    message     TEXT NOT NULL,
+    send_at     TIMESTAMP NOT NULL,
+    status      TEXT NOT NULL, -- pending, sent, failed, canceled
+    created_at  TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at  TIMESTAMP NOT NULL DEFAULT now()
+);
